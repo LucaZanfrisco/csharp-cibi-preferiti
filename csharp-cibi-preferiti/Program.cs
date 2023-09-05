dd20472ade7f41cc0814bb74473e09360da68d8d
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-05 15:54:13 +0200

    Stampa su terminale della lista dei cibi

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string[] cibi = { "Pasta", "Pizza", "Deliziosa", "Salmone" , "Spigola", "Polpo", "Cozze", "Vongole", "Lupini", "Platessa" };
+
+Console.WriteLine("La lunghezza della lista è: " + cibi.Length);
+Console.WriteLine("-----");
+
+for(int i = 0; i < cibi.Length; i++)
+{
+    Console.WriteLine(cibi[i] + " - " + (i+1));
+}
+
+Console.WriteLine("-----");
+Console.WriteLine("Top: " + cibi[0]);
+Console.WriteLine("Bottom: " + cibi[cibi.Length - 1]);
+
+
+if(cibi.Length % 2 == 0)
+{
+    Console.WriteLine("Middle: " + cibi[cibi.Length/2] + " - " + cibi[cibi.Length/2 - 1]);
+}
+else
+{
+    Console.WriteLine("Middle: " + cibi[cibi.Length / 2]);
+}
