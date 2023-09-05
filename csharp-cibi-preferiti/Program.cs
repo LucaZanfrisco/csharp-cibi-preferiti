bc4d30700f8ed4dbc244faa16f07aee60e20331b
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-05 16:07:41 +0200

    Refactor

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,25 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-string[] cibi = { "Pasta", "Pizza", "Deliziosa", "Salmone" , "Spigola", "Polpo", "Cozze", "Vongole", "Lupini", "Platessa" };
+string[] cibiPreferiti = { "Pasta", "Pizza", "Deliziosa", "Salmone" , "Spigola", "Polpo", "Cozze", "Vongole", "Lupini", "Platessa" };
 
-Console.WriteLine("La lunghezza della lista è: " + cibi.Length);
-Console.WriteLine("-----");
+var lungLista = cibiPreferiti.Length;
 
-for(int i = 0; i < cibi.Length; i++)
+Console.WriteLine("La lunghezza della lista è: " + lungLista + "\n");
+
+for(int i = 0; i < lungLista; i++)
 {
-    Console.WriteLine(cibi[i] + " - " + (i+1));
+    Console.WriteLine($"{cibiPreferiti[i]} - {(i + 1)}");
 }
 
-Console.WriteLine("-----");
-Console.WriteLine("Top: " + cibi[0]);
-Console.WriteLine("Bottom: " + cibi[cibi.Length - 1]);
+Console.WriteLine($"\nTop cibo preferito: {cibiPreferiti[0]}");
+Console.WriteLine($"Ultimo cibo preferito: {cibiPreferiti[lungLista - 1]} ");
 
-
-if(cibi.Length % 2 == 0)
+if(lungLista % 2 == 0)
 {
-    Console.WriteLine("Middle: " + cibi[cibi.Length/2] + " - " + cibi[cibi.Length/2 - 1]);
+    Console.WriteLine($"Cibi medio preferiti:  {cibiPreferiti[lungLista /2]} -  {cibiPreferiti[lungLista /2 - 1]}");
 }
 else
 {
-    Console.WriteLine("Middle: " + cibi[cibi.Length / 2]);
+    Console.WriteLine($"Cibo medio preferito: {cibiPreferiti[lungLista / 2]}");
 }
